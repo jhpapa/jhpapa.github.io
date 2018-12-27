41d299e27de61977bf941a79d87470e6ad4bd750
Sihong Park <pozt1234@gmail.com>
2018-12-27 15:17:24 +0900

    포스트 생성 스크립트 수정

--- init-post.sh
+++ init-post.sh
@@ -4,9 +4,7 @@
 BLOG_PATH=~/blog/hongsii.github.io
 TEMP_POST_PATH=$BLOG_PATH/_drafts
 
-
 read -p "타이틀 : " title
-
 post=$TEMP_POST_PATH/$title.md
 if [ -f $post ]; then
     echo "동일한 이름의 포스트가 존재합니다."
@@ -15,20 +13,24 @@ if [ -f $post ]; then
 fi
 
 read -p "설명 : " description
-read -p "날짜(YYYY-MM-DD HH24:MI) : " date
-read -p "카테고리 : " category
-read -p "태그 : " input_tags
-IFS=' ' read -a tags <<< "$input_tags"
 
+read -p "날짜(YYYY-MM-DD HH24:MI) : " date
+
+read -p "카테고리 : " category
+
+read -p "태그(공백으로 구분) : " input_tags
+IFS=' ' read -a tags <<< "$input_tags"
 formatted_tags=""
+index=0
 for tag in ${tags[@]}
 do
-    formatted_tags+="  - $tag"$'\n'
+    ((++index))
+    formatted_tags+="  - $tag"
+    if [[ index -ne ${#tags[@]} ]]; then
+       formatted_tags+=$'\n' 
+    fi
 done
 
-#touch $post
-
-#ls $post
 cat <<EOT >> $post
 ---
 layout: 'post'
@@ -41,3 +43,5 @@ tags:
 $formatted_tags
 ---
 EOT
+
+vim $post
